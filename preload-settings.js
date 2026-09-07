10ba3ff72d11f3e508de47e87375a1a5c4b10503
mscharbo-commits <mscharbo@gmail.com>
2026-09-06 21:58:22 -0400

    fix: add closeSettings to preload-settings

--- preload-settings.js
+++ preload-settings.js
@@ -1,5 +1,6 @@
 const { contextBridge, ipcRenderer } = require('electron');
 contextBridge.exposeInMainWorld('api', {
-  getSettings:  ()  => ipcRenderer.invoke('get-settings'),
-  saveSettings: (s) => ipcRenderer.invoke('save-settings', s),
+  getSettings:   ()  => ipcRenderer.invoke('get-settings'),
+  saveSettings:  (s) => ipcRenderer.invoke('save-settings', s),
+  closeSettings: ()  => ipcRenderer.invoke('close-settings'),
 });
