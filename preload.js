d291a32aea19a730d70f5e2c95cc390fc293c889
mscharbo-commits <mscharbo@gmail.com>
2026-09-06 21:18:13 -0400

    feat: preload.js with full API bridge

--- preload.js
+++ preload.js
@@ -6,5 +6,7 @@ contextBridge.exposeInMainWorld('api', {
   setStored:     (k,v)   => ipcRenderer.invoke('set-stored', k, v),
   openSettings:  ()      => ipcRenderer.invoke('open-settings'),
   openCryptikr:  (id)    => ipcRenderer.invoke('open-cryptikr', id),
+  openExternal:  (url)   => ipcRenderer.invoke('open-external', url),
   fetchPrices:   ()      => ipcRenderer.invoke('fetch-prices'),
+  quit:          ()      => ipcRenderer.invoke('quit'),
 });
